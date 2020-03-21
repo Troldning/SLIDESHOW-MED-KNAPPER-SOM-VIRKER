72da02f9660404569956242d140902bc4111dfa6
Troldning <62355367+Troldning@users.noreply.github.com>
2020-03-21 13:09:56 +0100

    Add project files.

---
+++ SLIDESHOW MED KNAPPER SOM VIRKER/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SLIDESHOW_MED_KNAPPER_SOM_VIRKER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.nextKnap = new System.Windows.Forms.Button();
+            this.prevKnap = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(103, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(582, 323);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // nextKnap
+            // 
+            this.nextKnap.Location = new System.Drawing.Point(610, 191);
+            this.nextKnap.Name = "nextKnap";
+            this.nextKnap.Size = new System.Drawing.Size(75, 23);
+            this.nextKnap.TabIndex = 1;
+            this.nextKnap.Text = "---->";
+            this.nextKnap.UseVisualStyleBackColor = true;
+            this.nextKnap.Click += new System.EventHandler(this.nextKnap_Click);
+            // 
+            // prevKnap
+            // 
+            this.prevKnap.Location = new System.Drawing.Point(103, 191);
+            this.prevKnap.Name = "prevKnap";
+            this.prevKnap.Size = new System.Drawing.Size(75, 23);
+            this.prevKnap.TabIndex = 2;
+            this.prevKnap.Text = "<----";
+            this.prevKnap.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Ligger ved mor-800px.jpg");
+            this.imageList1.Images.SetKeyName(1, "Løver i sne-800px.jpg");
+            this.imageList1.Images.SetKeyName(2, "Løveunger-800px.jpg");
+            this.imageList1.Images.SetKeyName(3, "Parring0_-800px.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.prevKnap);
+            this.Controls.Add(this.nextKnap);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button nextKnap;
+        private System.Windows.Forms.Button prevKnap;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ SLIDESHOW MED KNAPPER SOM VIRKER/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SLIDESHOW_MED_KNAPPER_SOM_VIRKER
+{
+    public partial class Form1 : Form
+    {
+        int _currentIndex = 0;
+        int currentIndex
+        {
+
+
+            get
+            {
+                return _currentIndex;
+            }
+            set
+            {
+                if (value >= imageList1.Images.Count)
+                {
+                    //    if index is out of range in the large side, reset to 0
+                    _currentIndex = 0;
+                }
+                else if (value < 0)
+                {
+                    //    if index is out of range in the samll side, reset to number of images - 1 (it's 0 based)
+                    _currentIndex = imageList1.Images.Count - 1;
+                }
+                else
+                {
+                    _currentIndex = value;
+                }
+                //    update image after index is updated
+                pictureBox1.Image = imageList1.Images[_currentIndex];
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = imageList1.Images[currentIndex];
+        }
+
+        private void nextKnap_Click(object sender, EventArgs e)
+        {
+            currentIndex++;
+        }
+
+        private void prevKnap_Click(object sender, EventArgs e)
+        {
+            currentIndex--;
+        }
+    }
+}
+    
